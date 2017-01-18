1875ef7bf508dc9e86bdb90d16bcae72b1811c21
William Winger <Billy_thekid55@hotmail.com>
2017-01-17 19:03:27 -0700

    changed nav

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -11,28 +11,21 @@ class NavBar extends React.Component {
 
   render() {
     return(
-      <nav className="nav-extended">
+      <nav className="nav-extended nav-fixed">
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Logo</a>
           <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
 
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="">Sass</a></li>
+            <li><a href="">Components</a></li>
+            <li><a href="">JavaScript</a></li>
           </ul>
 
           <ul className="side-nav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-
-          <ul className="tabs tabs-transparent">
-            <li className="tab"><a href="#test1">Test 1</a></li>
-            <li className="tab"><a className="active" href="#test2">Test 2</a></li>
-            <li className="tab disabled"><a href="#test3">Disabled Tab</a></li>
-            <li className="tab"><a href="#test4">Test 4</a></li>
+            <li><a href="">Sass</a></li>
+            <li><a href="">Components</a></li>
+            <li><a href="">JavaScript</a></li>
           </ul>
         </div>
       </nav>
